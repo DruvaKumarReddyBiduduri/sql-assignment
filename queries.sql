b2b8fb879006e1aca06e31d13739a28b4b4dbc70
Druva <druvabiduduri7@gmail.com>
2024-07-22 00:07:40 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -4,17 +4,23 @@ SELECT * FROM memberships;
 
 SELECT * FROM freezes;
 
+-- Insering dummy data into members table and membership table
 INSERT INTO members(name) VALUES('gojo');
 
-INSERT INTO memberships(start_date,end_date,member_id,duration) VALUES('2024-7-19',DATE_ADD(CURDATE(),INTERVAL 3 MONTH),1,3);
+-- Membership for 3 months
+INSERT INTO memberships(start_date,end_date,member_id,duration) VALUES(CURDATE(),DATE_ADD(CURDATE(),INTERVAL 3 MONTH),1,3);
 
+-- Data for freeze table
+-- Freeze membership for 7 days
+INSERT into freezes(no_of_days,start_date,end_date,member_id,membership_id) VALUES(7,CURDATE(),DATE_ADD(CURDATE(),INTERVAL 7 DAY),1,2)
+
+-- Freeze rules
+
+-- Checking for valid membership
 SELECT * FROM memberships 
 WHERE member_id=1 AND CURDATE()>=start_date AND CURDATE()<=end_date;
 
-
-INSERT into freezes(no_of_days,start_date,end_date,member_id,membership_id) VALUES(7,CURDATE(),DATE_ADD(CURDATE(),INTERVAL 7 DAY),1,2)
-
-
+-- Previous Total Freeze days
 SELECT * from freezes 
 WHERE (SELECT SUM(no_of_days) from freezes 
         WHERE member_id=1 and membership_id=2)<=53 
